3f36c86852117dccd803f1822e5fed5cab5d710a
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-09-23 17:01:27 -0600

    Add files via upload

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -71,7 +71,7 @@
         
 
         @foreach ($posts as $post)
-        <a href="#">
+        <a href="/posts/{{$post->id}}">
             <article class="border-b border-x border-gray-700 px-3 py-3 text-white text-base hover:bg-slate-800">
 
                     <div class="flex">
@@ -87,10 +87,12 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                                 </svg>
                                 <p class="ml-1 text-gray-400">@</p>
-                                <p class="text-gray-400">{{ $post->user->handle }} · 2h</p> 
+                                <p class="text-gray-400">{{ $post->user->handle }} · {{ $post->created_at->diffForHumans() }}</p> 
                             </div>
                          
-                            <p class="text-md">{{ $post->content }}</p>
+                            <a href="/posts/{{$post->id}}">
+                                <p class="text-md">{{ $post->content }}</p>
+                            </a>
                         </div>
                     </div>
     
