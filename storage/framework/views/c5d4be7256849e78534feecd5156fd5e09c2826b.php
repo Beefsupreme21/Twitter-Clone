1a604bcde223cdc0336ae34ad124c9e3a4096186
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-09-30 18:09:27 -0600

    Add files via upload

--- storage/framework/views/c5d4be7256849e78534feecd5156fd5e09c2826b.php
+++ storage/framework/views/c5d4be7256849e78534feecd5156fd5e09c2826b.php
@@ -210,8 +210,32 @@
                             </div>
                         </div>
 
+                        <?php $__currentLoopData = $retweets; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $retweet): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 
                         
+
+
+                        <?php if($retweet->user_id == auth()->id() && $post->id == $retweet->post_id): ?>
+                        <div class="flex text-green-500">
+                            <form action="/retweets/delete" method="POST">
+                                <?php echo csrf_field(); ?>
+                                <?php echo method_field('DELETE'); ?>
+
+                                <input type="hidden" name="post_id" value="<?php echo e($post->id); ?>">
+                                <button type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                                    </svg>          
+                                </button>
+                            </form>
+                            <p class="pl-3"><?php echo e($post->retweets_count); ?></p> 
+                        </div>                        
+                        
+                        <?php else: ?>
+                            
+                        <?php endif; ?>
+
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
                             <div class="flex hover:text-green-500">
                                 <form action="/retweets" method="POST">
@@ -240,10 +264,9 @@
                                         </svg>          
                                     </button>
                                 </form>
-
-                                
-
+                                <p class="pl-3"><?php echo e($post->retweets_count); ?></p> 
                             </div>
+
 
 
                         
